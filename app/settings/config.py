c329ce8442f4957c684a1c541fa0a494d80de07f
Mariusz Rusiniak <93149018+rusiniak@users.noreply.github.com>
2024-10-22 12:05:00 +0200

    removing outdated secrets

--- app/settings/config.py
+++ app/settings/config.py
@@ -1,6 +1,6 @@
-DB_URL = "postgresql+psycopg://postgres:TCtVsrSEXYBpOfJeURwvnJuprEmeOheg@junction.proxy.rlwy.net:27684/railway"
+DB_URL = ""
 
 
-SECRET_KEY = "job_board_app_secret!"
+SECRET_KEY = "j"
 ALGORITHM = "HS256"
 TOKEN_EXPIRATION_TIME_MINUTES = 15
